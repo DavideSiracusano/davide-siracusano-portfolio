613c46a29b5bb9171941e025426b509f82c93f0b
Davide Siracusano <152174921+DavideSiracusano@users.noreply.github.com>
2025-10-21 22:13:01 +0200

    fix responsive design

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -101,12 +101,15 @@ export default function Home() {
     >
       <div className="max-w-4xl mx-auto w-full">
         <div className="flex justify-center items-center mb-8 font-mono">
-          <h1 className="flex items-center justify-center sm:text-2xl md:text-3xl lg:text-4xl mx-auto">
-            <span className="html-tag shrink-0">&lt;h1&gt;</span>
-            <span className="code-content code-animation mx-2">
+          <h1
+            className="flex flex-col sm:flex-row items-center justify-center text-center
+               text-xl sm:text-2xl md:text-3xl lg:text-4xl max-w-full break-words"
+          >
+            <span className="html-tag shrink-0 mb-1 sm:mb-0">&lt;h1&gt;</span>
+            <span className="code-content code-animation mx-0 sm:mx-4">
               Benvenuto sulla mia pagina! 👋🏻
             </span>
-            <span className="html-tag shrink-0">&lt;/h1&gt;</span>
+            <span className="html-tag shrink-0 mt-1 sm:mt-0">&lt;/h1&gt;</span>
           </h1>
         </div>
 
